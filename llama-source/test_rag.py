840880174dee5b0bc53c039ec6135dd1c1704c28
lepagedart <jeremy.lepage@icloud.com>
2025-06-11 15:13:55 +0000

    Cleanup: removed temp llama-source folder

--- llama-source/test_rag.py
+++
@@ -1,5 +0,0 @@
-from rag_retriever import retrieve_codex_context
-
-context = retrieve_codex_context("What is the difference between a sour and a fizz?")
-print("\n=== Retrieved Context ===")
-print(context)
